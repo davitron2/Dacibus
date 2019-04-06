91b314ebfe15d35141d807390ae81959171b1ee6
NanaSnixx <anayelisnixx@gmail.com>
2019-04-05 19:49:02 -0700

    Update Utensilios.java

--- src/modelo/Utensilios.java
+++ src/modelo/Utensilios.java
@@ -2,8 +2,42 @@
 package modelo;
 
 public class Utensilios {
-    int IdUtensilios;
-    int Tipo;
-    int CantDispo;
-    int IdProducto;
+    private int IdUtensilios;
+    private int Tipo;
+    private int CantDispo;
+    private int IdProducto;
+
+    public int getIdUtensilios() {
+        return IdUtensilios;
+    }
+
+    public void setIdUtensilios(int IdUtensilios) {
+        this.IdUtensilios = IdUtensilios;
+    }
+
+    public int getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(int Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    public int getCantDispo() {
+        return CantDispo;
+    }
+
+    public void setCantDispo(int CantDispo) {
+        this.CantDispo = CantDispo;
+    }
+
+    public int getIdProducto() {
+        return IdProducto;
+    }
+
+    public void setIdProducto(int IdProducto) {
+        this.IdProducto = IdProducto;
+    }
+    
+    
 }
