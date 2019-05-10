a385be3e145f786a2d75320d79b600cd038ffefb
CessarDxD <Cesar_Her.Gon@hotmail.com>
2019-05-09 22:15:25 -0600

    Create PruebaRegistroUser.java
    Prueba 1 Registro usuarios

---
+++ test/Pruebas/PruebaRegistroUser.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pruebas;
+
+import modelo.Usuario;
+import modelo.UsuarioSQL;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cesar
+ */
+public class PruebaRegistroUser {
+    
+    public PruebaRegistroUser() {
+    
+    }
+    
+    @Test
+    public void testRegistro(){
+        modelo.UsuarioSQL ob=new UsuarioSQL();
+        
+        modelo.Usuario usr=new Usuario();
+        usr.setNombre("Cesar");
+        usr.setEdad(21);
+        usr.setGenero("H");
+        usr.setTipoUsuario("Administrdor");
+        usr.setUsuario("CesarH");
+        usr.setPass("123");
+        
+        
+        
+        int result=ob.Registrar(usr);
+        
+        int esperado=10;
+        
+        assertEquals(result,esperado);
+        
+    }
+   
+    
+   
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
